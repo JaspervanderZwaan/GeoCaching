407ae916646691ad666dfc588806545d0ef25eae
Poel,Jaron J.M <j.poel@student.fontys.nl>
2019-09-19 10:51:59 +0200

    Begin van UI

---
+++ GeoCaching/Controllers/MapController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GeoCaching.Models;
+
+namespace GeoCaching.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Map()
+        {
+            return View();
+        }
+    }
+}
---
+++ GeoCaching/Controllers/MessageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GeoCaching.Models;
+
+namespace GeoCaching.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
